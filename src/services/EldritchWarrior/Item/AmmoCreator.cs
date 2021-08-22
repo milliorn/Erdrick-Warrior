1633d2d9868f71f69d255bb905f320c93e52c1dd
Scott Milliorn <scottmilliorn@gmail.com>
2021-08-21 22:39:18 -0700

    itm_ammo_creator

---
+++ src/services/EldritchWarrior/Item/AmmoCreator.cs
@@ -0,0 +1,46 @@
+using Anvil.API;
+using Anvil.API.Events;
+using Anvil.Services;
+
+namespace EldritchWarrior.Item
+{
+    public class test
+    {
+        [ServiceBinding(typeof(IItemHandler))]
+        public class AmmoCreator : IItemHandler
+        {
+            public string Tag => "itm_ammo_creator";
+
+            public void HandleActivateItem(ModuleEvents.OnActivateItem item)
+            {
+                if (item.TargetObject is NwItem itemTarget && itemTarget.Tag == "itm_ammo_creator")
+                {
+                    switch (itemTarget.BaseItemType)
+                    {
+                        case BaseItemType.Armor:
+                        case BaseItemType.Bolt:
+                        case BaseItemType.Bullet:
+                            itemTarget.StackSize = 99;
+                            itemTarget.PlotFlag = true;
+                            break;
+                        case BaseItemType.Dart:
+                        case BaseItemType.Shuriken:
+                        case BaseItemType.ThrowingAxe:
+                            itemTarget.StackSize = 50;
+                            itemTarget.PlotFlag = true;
+                            break;
+                        case BaseItemType.Grenade:
+                            itemTarget.StackSize = 10;
+                            itemTarget.PlotFlag = true;
+                            break;
+                        default:
+                            NwCreature activator = item.ItemActivator;
+                            NwPlayer player = activator.LoginPlayer;
+                            player.DisplayFloatingTextStringOnCreature(activator, "The target must be Ammunition type item!");
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
